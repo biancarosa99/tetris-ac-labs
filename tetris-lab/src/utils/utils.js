351d8b242d0850afcd8647a388b21d3df1a6e8a6
biancarosa99 <bianca_rosa99@yahoo.com>
2022-05-11 18:48:21 +0300

    user can now rotate tetromino using up arrow key

--- tetris-lab/src/utils/utils.js
+++ tetris-lab/src/utils/utils.js
@@ -29,3 +29,13 @@ export const DIRECTION = {
   left: "left",
   right: "right",
 };
+
+export const getOppositeDirection = function (direction) {
+  if (direction === DIRECTION.down) {
+    return DIRECTION.up;
+  } else if (direction === DIRECTION.left) {
+    return DIRECTION.right;
+  } else {
+    return DIRECTION.left;
+  }
+};
